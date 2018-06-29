b0e5497a596a20c42696181a9367d76ebd646b28
richard-dds <richard-ctr@friends.dds.mil>
2018-06-29 14:52:37 -0400

    Update user

--- authz/api/__init__.py
+++ authz/api/__init__.py
@@ -44,6 +44,35 @@ def create_user():
     return UserSerializer().jsonify(user)
 
 
+@api.route("/users/<uuid:user_id>", methods=["PUT"])
+def update_user(user_id):
+    user_dict = request.json
+
+    try:
+        user = User.query.filter_by(id=user_id).one()
+    except NoResultFound:
+        abort(
+            Response(
+                {"error": "User {} not found.".format(user_id)}, 404
+            )
+        )
+
+    try:
+        atat_role = Role.query.filter_by(name=user_dict["atat_role"]).one()
+    except NoResultFound:
+        abort(
+            Response(
+                {"error": "Role {} not found.".format(user_dict["atat_role"])}, 404
+            )
+        )
+
+    user.atat_role = atat_role
+    db.session.add(user)
+    db.session.commit()
+
+    return UserSerializer().jsonify(user)
+
+
 @api.route("/workspaces/<uuid:workspace_id>/users", methods=["PUT"])
 def update_workspace_users(workspace_id):
     """
--- tests/api/v1/test_roles.py
+++ tests/api/v1/test_roles.py
@@ -33,6 +33,47 @@ def test_create_user(client):
     assert response.json["atat_role"] == "ccpo"
 
 
+def test_update_user(client):
+    user_id = str(uuid4())
+    client.post(
+        "/users",
+        content_type="application/json",
+        data=dumps({"id": user_id, "atat_role": "ccpo"}),
+    )
+    response = client.put(
+        "/users/{}".format(user_id),
+        content_type="application/json",
+        data=dumps({"atat_role": "developer"}),
+    )
+    assert response.status_code == 200
+    assert response.json["id"] == user_id
+    assert response.json["atat_role"] == "developer"
+
+
+def test_update_nonexistent_user(client):
+    user_id = str(uuid4())
+    response = client.put(
+        "/users/{}".format(user_id),
+        content_type="application/json",
+        data=dumps({"atat_role": "developer"}),
+    )
+    assert response.status_code == 404
+
+
+def test_update_user_with_nonexistent_role(client):
+    user_id = str(uuid4())
+    client.post(
+        "/users",
+        content_type="application/json",
+        data=dumps({"id": user_id, "atat_role": "ccpo"}),
+    )
+    response = client.put(
+        "/users/{}".format(user_id),
+        content_type="application/json",
+        data=dumps({"atat_role": "invalid_role"}),
+    )
+    assert response.status_code == 404
+
 def test_update_workspace_user_roles(client):
     workspace_id = str(uuid4())
     user1_id = str(uuid4())
