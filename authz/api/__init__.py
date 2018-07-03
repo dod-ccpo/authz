95a7e42d69fa88d1db497f93466cd553b24f8528
richard-dds <richard-ctr@friends.dds.mil>
2018-07-02 20:31:32 -0400

    Add some docstrings to the api views

--- authz/api/__init__.py
+++ authz/api/__init__.py
@@ -12,6 +12,11 @@ api = Blueprint("api", __name__)
 
 @api.route("/roles")
 def get_roles():
+    """
+    Returns a list of all existing roles.
+
+    GET /roles
+    """
     roles = Role.query.all()
     return RoleSerializer().jsonify(roles, many=True)
 
@@ -27,6 +32,17 @@ def get_role(name):
 
 @api.route("/users", methods=["POST"])
 def create_user():
+    """
+    Creates a new atat user with the given role.
+
+    POST /users/
+    {
+        id: <user id>,
+        atat_role: <name of an atat role>
+    }
+
+    Returns the created user.
+    """
     user_dict = request.json
 
     try:
@@ -50,6 +66,16 @@ def create_user():
 
 @api.route("/users/<uuid:user_id>", methods=["PUT"])
 def update_user(user_id):
+    """
+    Update a user's atat role.
+
+    PUT /users/<user id>
+    {
+        atat_role: <name of an atat role>
+    }
+
+    Returns the updated user.
+    """
     user_dict = request.json
 
     try:
@@ -76,11 +102,16 @@ def update_user(user_id):
 @api.route("/workspaces/<uuid:workspace_id>/users", methods=["PUT"])
 def update_workspace_users(workspace_id):
     """
+    Add or update users' workspace roles in a given workspace.
+
+    PUT /workspaces/<workspace id>/users
     {
         'users': [
             {'id': '', 'workspace_role': 'developer'}
         ]
     }
+
+    Returns a list of users who have roles in the given workspace.
     """
 
     workspace_users_to_update = request.json["users"]
@@ -117,6 +148,13 @@ def update_workspace_users(workspace_id):
 
 @api.route("/workspaces/<uuid:workspace_id>/users/<uuid:user_id>", methods=["GET"])
 def get_workspace_user(workspace_id, user_id):
+    """
+    Get a user, along with that user's permissions in the given workspace.
+
+    GET /workspaces/<workspace id>/users/<user id>
+
+    Returns a user.
+    """
     try:
         user = User.query.filter_by(id=user_id).one()
     except NoResultFound:
